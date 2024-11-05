dae187179f0f9649e5b887a68732181a27a6b4df
Matthew Choi <matthewchoi@MacBook-Pro-5.local>
2024-11-05 11:30:04 -0500

    major update

---
+++ fastapi/app/schemas/predict.py
@@ -0,0 +1,34 @@
+from typing import Any, List, Optional
+
+from pydantic import BaseModel
+from red_wine_model.processing.validation import WineDataInputSchema
+
+
+class PredictionResults(BaseModel):
+    errors: Optional[Any]
+    version: str
+    predictions: Optional[List[float]]
+
+
+class MultipleHouseDataInputs(BaseModel):
+    inputs: List[WineDataInputSchema]
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "inputs": [
+                    {
+                        'fixed acidity': 7.4,
+                        'volatile acidity': 0.25,
+                        'citric acid': 0.29,
+                        'residual sugar': 2.2,
+                        'chlorides': 0.054, 
+                        'free sulfur dioxide': 19.0,
+                        'total sulfur dioxide': 49.0,
+                        'sulphates': 0.76,
+                        'alcohol': 10.9
+                        # output should be "good" == 1
+                    }
+                ]
+            }
+        }
---
+++ prod_env/tests/conftest.py
@@ -0,0 +1,27 @@
+import logging
+
+import pytest
+from sklearn.model_selection import train_test_split
+
+from red_wine_model.config.core import config
+from red_wine_model.processing.data_manager import load_dataset
+
+logger = logging.getLogger(__name__)
+
+
+@pytest.fixture
+def sample_input_data():
+    data = load_dataset(file_name=config.app_config.raw_data_file)
+
+    # divide train and test
+    X_train, X_test, y_train, y_test = train_test_split(
+        data[config.m_config.features],  # predictors
+        data[config.m_config.target],
+        test_size=config.m_config.test_size,
+        # we are setting the random seed here
+        # for reproducibility
+        random_state=config.m_config.random_state,
+        stratify=data[config.m_config.target]
+    )
+
+    return X_test, y_test
